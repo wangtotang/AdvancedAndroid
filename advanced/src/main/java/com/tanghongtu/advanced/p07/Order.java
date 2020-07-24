ccc74a520c90acc443a4c69a7a3fec2a136a7d12
tanghongtu <tanghongtu@ut.cn>
2020-07-24 09:38:43 +0800

    add:p07

---
+++ advanced/src/main/java/com/tanghongtu/advanced/p07/Order.java
@@ -0,0 +1,14 @@
+package com.tanghongtu.advanced.p07;
+
+/**
+ * Created by tanghongtu on 2020/7/24.
+ */
+public class Order {
+
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+        a = a + 1;
+    }
+
+}
