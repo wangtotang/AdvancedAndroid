5d24c5030c07a40c135e21b71fa1ac0fe9534b06
tanghongtu <tanghongtu@ut.cn>
2020-08-09 22:34:31 +0800

    commit p23

--- app/src/main/java/com/tanghongtu/advanced/MainActivity.java
+++ app/src/main/java/com/tanghongtu/advanced/MainActivity.java
@@ -1,12 +1,36 @@
 package com.tanghongtu.advanced;
 
-import android.content.Intent;
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static Runnable myRunnable = new Runnable() {
+
+        private Handler handler;
+
+        @SuppressLint("HandlerLeak")
+        @Override
+        public void run() {
+            //创建子线程的looper和绑定handler子对象
+            Looper.prepare();
+            handler = new Handler() {
+
+                @Override
+                public void handleMessage(@NonNull Message msg) {
+
+                }
+            };
+            Looper.loop();
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,4 +40,5 @@ public class MainActivity extends AppCompatActivity {
         viewById.setText("怎么回事？");
 
     }
+
 }
